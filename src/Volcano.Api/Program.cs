d43e2f5b1814d0a5dff8b1c046b6092dc8bcd7a2
Maciej Kachniarz <mkachniarz1@gmail.com>
2023-08-05 21:48:55 +0200

    add sample api handler

--- src/Volcano.Api/Program.cs
+++ src/Volcano.Api/Program.cs
@@ -1,7 +1,11 @@
+using MediatR;
+using Volcano.Core.Requests;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfn => cfn.RegisterServicesFromAssemblyContaining<GetTyreRequest>());
 
 var app = builder.Build();
 
@@ -12,8 +16,6 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapGet("/tires", () => "XD");
+app.MapGet("/tires/{id}", (int id, IMediator _mediator) => _mediator.Send(new GetTyreRequest(id)));
 
 app.Run();
---
+++ src/Volcano.Core/Requests/GetTyreRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Volcano.Core.Requests;
+
+public class GetTyreRequest : IRequest<int>
+{
+    public int TyreId { get; private set; }
+    
+    public GetTyreRequest(int id) 
+        => TyreId = id;
+}
